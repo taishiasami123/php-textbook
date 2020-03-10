23989e03d80f696d583a6bc4c693105d80cdf6d3
taishiasami123 <taishi.asami123@gmail.com>
2020-03-10 14:18:23 +0900

    練習問題をコミット

---
+++ practice/3-10.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="css/style.css">
+  <title>よくわかるPHPの教科書</title>
+</head>
+
+<body>
+  <header>
+    <h1 class="font-weight-normal">よくわかるPHPの教科書</h1>
+  </header>
+
+  <main>
+    <h2>Practice</h2>
+    <pre>
+      <?php
+        $answer = 0;
+        if ($answer == 0) {
+          print('1以上の数字を指定してください');
+        }
+      ?>
+    </pre>
+  </main>
+</body>
+</html>
---
+++ practice/3-17.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="css/style.css">
+  <title>よくわかるPHPの教科書</title>
+</head>
+
+<body>
+  <header>
+    <h1 class="font-weight-normal">よくわかるPHPの教科書</h1>
+  </header>
+
+  <main>
+    <h2>Practice</h2>
+    <pre>
+      <dl>
+        <dt>お名前</dt>
+          <dd><?php print(htmlspecialchars($_GET['my_name'], ENT_QUOTES)); ?></dd>
+        <dt>メッセージ</dt>
+          <dd><?php print(htmlspecialchars($_GET['message'], ENT_QUOTES)); ?></dd>
+      </dl>
+    </pre>
+  </main>
+</body>
+</html>
